99fcde4d635fb14dd9663eb233e8eeac9d4b04cd
Rayan Victor <69490855+Rayan-1@users.noreply.github.com>
2023-11-08 12:03:15 -0300

    Create mt.py

---
+++ mt.py
@@ -0,0 +1,61 @@
+def is_valid_string(input_string):
+    tape = list(input_string)
+    tape.append(' ')
+
+    state = 'q0'
+    head = 0
+
+    while True:
+        symbol = tape[head]
+
+        if state == 'q0':
+            if symbol == '0':
+                tape[head] = 'X'
+                head += 1
+                state = 'q0'
+            elif symbol == '1':
+                tape[head] = 'X'
+                head += 1
+                state = 'q0'
+            elif symbol == '#':
+                tape[head] = '#'
+                head += 1
+                state = 'q1'
+            else:
+                return False
+
+        elif state == 'q1':
+            if symbol == '0':
+                tape[head] = '0'
+                head += 1
+                state = 'q1'
+            elif symbol == '1':
+                tape[head] = '1'
+                head += 1
+                state = 'q1'
+            elif symbol == 'X':
+                tape[head] = 'X'
+                head += 1
+                state = 'q1'
+            elif symbol == ' ':
+                state = 'q2'
+            else:
+                return False
+
+        elif state == 'q2':
+            if symbol == ' ':
+                return True
+            else:
+                return False
+
+# Função que verifica se uma string está na linguagem
+def verifica_linguagem(input_string):
+    if is_valid_string(input_string):
+        return "A entrada está na linguagem."
+    else:
+        return "A entrada não está na linguagem."
+
+# Exemplo de uso
+input_string = input("Digite a string para verificar se está na linguagem: ")
+resultado = verifica_linguagem(input_string)
+print(resultado)
