2839251ed9c3dc1247ae0421fa7562dfe1b8b7eb
Rayan Victor <69490855+Rayan-1@users.noreply.github.com>
2023-11-08 13:16:59 -0300

    Update mt.py

--- mt.py
+++ mt.py
@@ -1,61 +1,66 @@
-def entrada(input_string):
-    fita = list(input_string)
-    fita.append(' ')
-    #estado inicial
-    state = 'q0'
-    #cabeça
-    head = 0
+class MaquinaTuring:
+    def __init__(self, estados, alfabeto, branco, estado_inicial, estado_aceitacao, estado_rejeicao, transicoes):
+        self.estados = estados
+        self.alfabeto = alfabeto
+        self.branco = branco
+        self.estado_inicial = estado_inicial
+        self.estado_aceitacao = estado_aceitacao
+        self.estado_rejeicao = estado_rejeicao
+        self.transicoes = transicoes
+        self.fitat = [branco]
+        self.cabeca = 0
+        self.estado_atual = estado_inicial
 
-    while True:
-        symbol = fita[head]
-        if state == 'q0':
-            if symbol == '0':
-                fita[head] = 'X'
-                head += 1
-                state = 'q0'
-            elif symbol == '1':
-                fita[head] = 'X'
-                head += 1
-                state = 'q0'
-            elif symbol == '#':
-                fita[head] = '#'
-                head += 1
-                state = 'q1'
-            else:
-                return False
+    def executar(self, entrada):
+        self.fitat = [self.branco] + list(entrada) + [self.branco]
+        self.cabeca = 1
+        self.estado_atual = self.estado_inicial
 
-        elif state == 'q1':
-            if symbol == '0':
-                fita[head] = '0'
-                head += 1
-                state = 'q1'
-            elif symbol == '1':
-                fita[head] = '1'
-                head += 1
-                state = 'q1'
-            elif symbol == 'X':
-                fita[head] = 'X'
-                head += 1
-                state = 'q1'
-            elif symbol == ' ':
-                state = 'q2'
-            else:
-                return False
+        while self.estado_atual != self.estado_aceitacao and self.estado_atual != self.estado_rejeicao:
+            simbolo_atual = self.fitat[self.cabeca]
+            if (self.estado_atual, simbolo_atual) not in self.transicoes:
+                break
+            proximo_estado, simbolo_escrito, direcao_movimento = self.transicoes[(self.estado_atual, simbolo_atual)]
+            self.fitat[self.cabeca] = simbolo_escrito
+            if direcao_movimento == 'E':
+                self.cabeca -= 1
+            elif direcao_movimento == 'D':
+                self.cabeca += 1
+            self.estado_atual = proximo_estado
 
-        elif state == 'q2':
-            if symbol == ' ':
-                return True
-            else:
-                return False
+        return self.estado_atual == self.estado_aceitacao
 
-# Função que verifica se uma string está na linguagem
-def verifica_linguagem(input_string):
-    if entrada(input_string):
-        return "A entrada está na linguagem."
-    else:
-        return "A entrada não está na linguagem."
+# Definindo estados, símbolos e funções de transição
+estados = {'q0', 'q1', 'q2', 'q3', 'q4'}
+alfabeto = {'0', '1', '#', 'X', '_'}
+branco = '_'
+estado_inicial = 'q0'
+estado_aceitacao = 'q3'
+estado_rejeicao = 'q4'
+transicoes = {
+    ('q0', '0'): ('q1', 'X', 'D'),
+    ('q0', '1'): ('q1', 'X', 'D'),
+    ('q0', '#'): ('q2', '#', 'S'),
+    ('q1', '0'): ('q1', '0', 'D'),
+    ('q1', '1'): ('q1', '1', 'D'),
+    ('q1', '#'): ('q2', '#', 'S'),
+    ('q2', '#'): ('q3', '#', 'S'),
+    ('q1', '_'): ('q4', '_', 'S'),
+    ('q2', '_'): ('q4', '_', 'S'),
+    ('q0', '_'): ('q3', '_', 'S')
+}
 
-# Exemplo de uso
-input_string = input("Digite a string para verificar se está na linguagem: ")
-resultado = verifica_linguagem(input_string)
-print(resultado)
+# Criar uma instância da Máquina de Turing
+mt = MaquinaTuring(estados, alfabeto, branco, estado_inicial, estado_aceitacao, estado_rejeicao, transicoes)
+
+# Solicitar entrada ao usuário
+entrada = input("Digite a string para verificar se está na linguagem: ")
+
+# Executar a Máquina de Turing na entrada fornecida
+resultado = mt.executar(entrada)
+
+# Exibir o resultado
+if resultado:
+    print("A entrada está na linguagem.")
+else:
+    print("A entrada não está na linguagem.")
